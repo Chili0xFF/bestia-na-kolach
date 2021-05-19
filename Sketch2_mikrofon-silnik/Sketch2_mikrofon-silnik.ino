d4714f4e80f010e07bb83bc417ff933730f31e20
Artur Szulist <75846282+Chili0xFF@users.noreply.github.com>
2021-05-19 21:17:41 +0200

    Dodane resety
    Po wyłączeniu mikrofonu, oraz przed jego włączeniem będzie się resetować do LOW. Po włączeniu mikrofonu, przestaje się resetować, zaczyna wykrywać dźwięk

--- Sketch2_mikrofon-silnik/Sketch2_mikrofon-silnik.ino
+++ Sketch2_mikrofon-silnik/Sketch2_mikrofon-silnik.ino
@@ -4,7 +4,7 @@ int micro_in = 9,micro_out =10;
 void setup()
 {
   pinMode(arduino_1,INPUT); ///Input z pierwszego arduino. Jeśli 1, uruchamia wykrywanie mikrofonu
-  
+  pinMode(10,OUTPUT);
   pinMode(9,INPUT);
   Serial.begin(9600);
 }
@@ -25,11 +25,11 @@ void microphone(int in, int out){
     //Serial.print("Mikrofon input: ");Serial.println(val);
     if(val==0){
       Serial.println("Jedziemy");
-      digitalWrite(out,LOW);
+      digitalWrite(out,HIGH);
       delay(3000);
       }
      else if(val==1){
       Serial.println("Nie jedziemy!");
-      digitalWrite(out,HIGH);
+      digitalWrite(out,LOW);
       }
   }
